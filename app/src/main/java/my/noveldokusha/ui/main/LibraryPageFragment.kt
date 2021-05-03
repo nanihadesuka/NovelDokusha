2fa53ffd8daacd5e4fdb514b0ba20c0e1247f348
nani <nani>
2021-05-04 00:05:00 +0200

    Refactor LibraryPageFragment.kt

--- app/src/main/java/my/noveldokusha/ui/main/LibraryPageFragment.kt
+++ app/src/main/java/my/noveldokusha/ui/main/LibraryPageFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DiffUtil
@@ -20,9 +20,10 @@ import my.noveldokusha.R
 import my.noveldokusha.bookstore
 import my.noveldokusha.databinding.ActivityMainFragmentLibraryPageBinding
 import my.noveldokusha.databinding.ActivityMainFragmentLibraryPageGridviewItemBinding
+import my.noveldokusha.ui.BaseFragment
 import my.noveldokusha.ui.chaptersList.ChaptersActivity
 
-class LibraryPageFragment : Fragment()
+class LibraryPageFragment : BaseFragment()
 {
 	companion object
 	{
@@ -44,7 +45,7 @@ class LibraryPageFragment : Fragment()
 	
 	private inner class Adapter
 	{
-		val gridView by lazy { NovelItemAdapter(viewModel.books) }
+		val gridView by lazy { NovelItemAdapter(this@LibraryPageFragment, viewModel.books) }
 	}
 	
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
@@ -67,16 +68,6 @@ class LibraryPageFragment : Fragment()
 		}
 		
 		return viewHolder.root
-	}
-	
-	private fun completedDialog(book: bookstore.Book) = MaterialDialog(requireActivity()).show {
-		title(text = book.title)
-		checkBoxPrompt(text = "Completed", isCheckedDefault = book.completed) {}
-		negativeButton(text = "Cancel")
-		positiveButton(text = "Ok") {
-			val completed = isCheckPromptChecked()
-			lifecycleScope.launch { bookstore.bookLibrary.update(book.copy(completed = completed)) }
-		}
 	}
 	
 	private fun notifyUpdated(text: String)
@@ -105,55 +96,78 @@ class LibraryPageFragment : Fragment()
 		}
 	}
 	
-	inner class NovelItemAdapter(private val list: MutableList<LibraryPageModel.BookItem>) :
-		RecyclerView.Adapter<NovelItemAdapter.ViewBinder>()
+}
+
+private class NovelItemAdapter(
+	private val context: BaseFragment,
+	private val list: MutableList<LibraryPageModel.BookItem>
+) : RecyclerView.Adapter<NovelItemAdapter.ViewBinder>()
+{
+	private inner class Diff(private val new: List<LibraryPageModel.BookItem>) : DiffUtil.Callback()
 	{
-		private inner class Diff(private val new: List<LibraryPageModel.BookItem>) : DiffUtil.Callback()
+		override fun getOldListSize(): Int = list.size
+		override fun getNewListSize(): Int = new.size
+		override fun areItemsTheSame(oldPos: Int, newPos: Int): Boolean = list[oldPos].data.url == new[newPos].data.url
+		override fun areContentsTheSame(oldPos: Int, newPos: Int): Boolean = list[oldPos].data == new[newPos].data
+	}
+	
+	fun setList(newList: List<LibraryPageModel.BookItem>) = DiffUtil.calculateDiff(Diff(newList)).let {
+		val isEmpty = list.isEmpty()
+		list.clear()
+		list.addAll(newList)
+		if (isEmpty) notifyDataSetChanged() else it.dispatchUpdatesTo(this)
+	}
+	
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewBinder
+	{
+		return ViewBinder(ActivityMainFragmentLibraryPageGridviewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+	}
+	
+	override fun getItemCount() = list.size
+	
+	override fun onBindViewHolder(binder: ViewBinder, position: Int)
+	{
+		val viewModel = this.list[position]
+		val viewHolder = binder.viewHolder
+		binder.setObservers(viewModel)
+		
+		viewHolder.title.text = viewModel.data.title
+		viewHolder.unreadChaptersCounter.visibility = View.INVISIBLE
+		viewHolder.book.setOnClickListener {
+			val intent = ChaptersActivity.Extras(bookUrl = viewModel.data.url, bookTitle = viewModel.data.title)
+				.intent(context.requireActivity())
+			context.startActivity(intent)
+		}
+		viewHolder.book.setOnLongClickListener {
+			completedDialog(context, viewModel.data)
+			true
+		}
+	}
+	
+	inner class ViewBinder(val viewHolder: ActivityMainFragmentLibraryPageGridviewItemBinding) : RecyclerView.ViewHolder(viewHolder.root)
+	{
+		var viewModelLast: LibraryPageModel.BookItem? = null
+		val numberOfUnreadChaptersObserver = Observer<Int> {
+			viewHolder.unreadChaptersCounter.text = it.toString()
+			viewHolder.unreadChaptersCounter.visibility = if (it == 0) View.INVISIBLE else View.VISIBLE
+		}
+		
+		fun setObservers(viewModel: LibraryPageModel.BookItem)
 		{
-			override fun getOldListSize(): Int = list.size
-			override fun getNewListSize(): Int = new.size
-			override fun areItemsTheSame(oldPos: Int, newPos: Int): Boolean = list[oldPos].data.url == new[newPos].data.url
-			override fun areContentsTheSame(oldPos: Int, newPos: Int): Boolean = list[oldPos].data == new[newPos].data
+			viewModelLast?.numberOfUnreadChapters?.removeObserver(numberOfUnreadChaptersObserver)
+			viewModel.numberOfUnreadChapters.observe(context.viewLifecycleOwner, numberOfUnreadChaptersObserver)
+			viewModelLast = viewModel
 		}
 		
-		fun setList(newList: List<LibraryPageModel.BookItem>) = DiffUtil.calculateDiff(Diff(newList)).let {
-			val isEmpty = list.isEmpty()
-			list.clear()
-			list.addAll(newList)
-			if (isEmpty) notifyDataSetChanged() else it.dispatchUpdatesTo(this)
-		}
-		
-		override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewBinder
-		{
-			return ViewBinder(ActivityMainFragmentLibraryPageGridviewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-		}
-		
-		override fun getItemCount() = list.size
-		
-		override fun onBindViewHolder(binder: ViewBinder, position: Int)
-		{
-			val itemData = this.list[position]
-			val itemView = binder.viewHolder
-			itemView.title.text = itemData.data.title
-			itemView.unreadChaptersCounter.visibility = View.INVISIBLE
-			
-			itemData.numberOfUnreadChapters.removeObservers(viewLifecycleOwner)
-			itemData.numberOfUnreadChapters.observe(viewLifecycleOwner) {
-				itemView.unreadChaptersCounter.text = it.toString()
-				itemView.unreadChaptersCounter.visibility = if (it == 0) View.INVISIBLE else View.VISIBLE
-			}
-			
-			itemView.book.setOnClickListener {
-				val intent = ChaptersActivity.Extras(bookUrl = itemData.data.url, bookTitle = itemData.data.title).intent(requireActivity())
-				this@LibraryPageFragment.startActivity(intent)
-			}
-			
-			itemView.book.setOnLongClickListener {
-				completedDialog(itemData.data)
-				true
-			}
-		}
-		
-		inner class ViewBinder(val viewHolder: ActivityMainFragmentLibraryPageGridviewItemBinding) : RecyclerView.ViewHolder(viewHolder.root)
+	}
+}
+
+private fun completedDialog(context: BaseFragment, book: bookstore.Book) = MaterialDialog(context.requireActivity()).show {
+	title(text = book.title)
+	checkBoxPrompt(text = "Completed", isCheckedDefault = book.completed) {}
+	negativeButton(text = "Cancel")
+	positiveButton(text = "Ok") {
+		val completed = isCheckPromptChecked()
+		context.lifecycleScope.launch { bookstore.bookLibrary.update(book.copy(completed = completed)) }
 	}
 }
--- app/src/main/java/my/noveldokusha/ui/reader/ReaderActivity.kt
+++ app/src/main/java/my/noveldokusha/ui/reader/ReaderActivity.kt
@@ -5,10 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Typeface
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.*
 import android.widget.AbsListView
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
