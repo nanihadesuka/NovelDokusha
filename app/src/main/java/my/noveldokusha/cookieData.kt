92794575569989ee3451d310ab6ea68b0a5c40ca
nani <nani>
2021-06-12 15:31:32 +0200

    CookieData. Use ConcurrentHashMap for the parts that get accessed by multiple coroutines.

--- app/src/main/java/my/noveldokusha/cookieData.kt
+++ app/src/main/java/my/noveldokusha/cookieData.kt
@@ -6,34 +6,39 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import my.noveldokusha.scraper.toUrl
 import java.io.File
+import java.util.concurrent.ConcurrentHashMap
 
-private typealias mmm = MutableMap<String, MutableMap<String, String>>
+private typealias CHMap = ConcurrentHashMap<String, ConcurrentHashMap<String, String>>
+private typealias MM = MutableMap<String, MutableMap<String, String>>
 
 class Data(val name: String)
 {
 	private val file = File(App.cacheDir, name)
 	
-	private fun fileGet(): mmm = GsonBuilder().create().fromJson(file.readText())
-	private fun fileSet(value: mmm): Unit = file.writeText(GsonBuilder().create().toJson(value))
+	private fun fileGet(): CHMap = GsonBuilder().create().fromJson<MM>(file.readText())
+		.mapValues { ConcurrentHashMap(it.value) }
+		.let { ConcurrentHashMap(it) }
 	
-	private val list = mutableMapOf<String, MutableMap<String, String>>()
+	private fun fileSet(value: CHMap): Unit = value.mapValues { it.value.toMap() }
+		.toMap()
+		.let { file.writeText(GsonBuilder().create().toJson(it)) }
+	
+	private val list = CHMap()
 	private val scope = CoroutineScope(Dispatchers.IO)
 	
 	fun load() = scope.launch {
 		// Here we play with the fact the application will have enough
 		// time to load the data before is actually used, so we don't
 		// worry if loads in another thread.
-		if (!file.exists()) fileSet(list)
-		else list.putAll(fileGet())
+		if (file.exists())
+			list.putAll(fileGet())
 	}
 	
 	fun add(url: String, values: Map<String, String>)
 	{
 		val domainName = url.toUrl().authority!!
-		list.getOrPut(domainName) { mutableMapOf() }.putAll(values)
-		val copy = mutableMapOf<String, MutableMap<String, String>>()
-		copy.putAll(list)
-		scope.launch { fileSet(copy) }
+		list.getOrPut(domainName) { ConcurrentHashMap() }.putAll(values)
+		scope.launch { fileSet(list) }
 	}
 	
 	fun get(url: String): Map<String, String>
