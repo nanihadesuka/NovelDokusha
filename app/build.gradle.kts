121e72709e1b9e1874e47a5359bab5711a5a49e3
nani <nani>
2021-09-23 21:36:11 +0200

    Version bump.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "my.noveldokusha"
         minSdk = 26
         targetSdk = 30
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
         setProperty("archivesBaseName", "NovelDokusha_v$versionName")
     }
 
