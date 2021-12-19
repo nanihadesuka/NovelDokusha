512417c31f888faa0926e6ee47fca56725b18bd8
nani <87673002+nanihadesuka@users.noreply.github.com>
2021-12-19 14:02:10 +0100

    Update material and material-dialogs libs.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,10 +110,10 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.2")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("com.google.android.material:material:1.4.0")
+    implementation("com.google.android.material:material:1.6.0-alpha01")
     implementation("com.l4digital.fastscroll:fastscroll:2.0.1")
-    implementation("com.afollestad.material-dialogs:core:3.2.1")
-    
+    implementation("com.afollestad.material-dialogs:core:3.3.0")
+
     // Glide
     implementation("com.github.bumptech.glide:glide:4.12.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
