6c44a36c0ddbe331b43452642ef4c29ebddd7e1c
nani <87673002+nanihadesuka@users.noreply.github.com>
2022-04-07 01:28:14 +0200

    Fix some automated blocking by the website, standard scrapping.

--- app/src/main/java/my/noveldokusha/scraper/sources/_1stKissNovel.kt
+++ app/src/main/java/my/noveldokusha/scraper/sources/_1stKissNovel.kt
@@ -42,52 +42,52 @@ class _1stKissNovel : SourceInterface.catalog
 
     override suspend fun getCatalogList(index: Int): Response<List<BookMetadata>>
     {
-        val url = "https://1stkissnovel.love/wp-admin/admin-ajax.php"
+        val page = index + 1
         return tryConnect {
-            connect(url)
-                .addHeaderRequest()
-                .data("action", "madara_load_more")
-                .data("page", index.toString())
-                .data("template", "madara-core/content/content-archive")
-                .data("vars[paged]", "1")
-                .data("vars[orderby]", "post_title")
-                .data("vars[template]", "archive")
-                .data("vars[sidebar]", "right")
-                .data("vars[post_type]", "wp-manga")
-                .data("vars[post_status]", "publish")
-                .data("vars[order]", "ASC")
-                .data("vars[meta_query][relation]", "OR")
-                .data("vars[manga_archives_item_layout]", "default")
-                .postIO()
-                .select(".post-title.font-title")
+
+            val url = "https://1stkissnovel.love"
+                .toUrlBuilderSafe()
+                .ifCase(page == 1){ addPath("page", page.toString())}
+
+            fetchDoc(url, 10000)
+                .select(".page-item-detail")
                 .mapNotNull { it.selectFirst("a[href]") }
-                .map { BookMetadata(title = it.text(), url = it.attr("href")) }
+                .map {
+                    val coverImageUrl = it.selectFirst("img[data-src]")?.attr("data-src")
+                    BookMetadata(
+                        title = it.attr("title"),
+                        url = it.attr("href"),
+                        coverImageUrl = coverImageUrl ?: "",
+                    )
+                }
                 .let { Response.Success(it) }
         }
     }
 
     override suspend fun getCatalogSearch(index: Int, input: String): Response<List<BookMetadata>>
     {
-        val url = "https://1stkissnovel.love/wp-admin/admin-ajax.php"
+        val page = index + 1
         return tryConnect {
-            connect(url)
-                .addHeaderRequest()
-                .data("action", "madara_load_more")
-                .data("page", index.toString())
-                .data("template", "madara-core/content/content-search")
-                .data("vars[s]", input)
-                .data("vars[orderby]", "")
-                .data("vars[paged]", "1")
-                .data("vars[template]", "search")
-                .data("vars[meta_query][0][relation]", "AND")
-                .data("vars[meta_query][relation]", "OR")
-                .data("vars[post_type]", "wp-manga")
-                .data("vars[post_status]", "publish")
-                .data("vars[manga_archives_item_layout]", "default")
-                .postIO()
-                .select("div.post-title")
+
+            val url = "https://1stkissnovel.love"
+                .toUrlBuilderSafe()
+                .ifCase(page == 1){ addPath("page", page.toString())}
+                .add("s",input)
+                .add("post_type","wp-manga")
+                .toString()
+                .plus("&op&author&artist&release&adult&m_orderby")
+
+            fetchDoc(url, 10000)
+                .select(".row.c-tabs-item__content")
                 .mapNotNull { it.selectFirst("a[href]") }
-                .map { BookMetadata(title = it.text(), url = it.attr("href")) }
+                .map {
+                    val coverImageUrl = it.selectFirst("img[data-src]")?.attr("data-src")
+                    BookMetadata(
+                        title = it.attr("title"),
+                        url = it.attr("href"),
+                        coverImageUrl = coverImageUrl ?: "",
+                    )
+                }
                 .let { Response.Success(it) }
         }
     }
--- app/src/main/java/my/noveldokusha/scraper/utils.kt
+++ app/src/main/java/my/noveldokusha/scraper/utils.kt
@@ -17,6 +17,11 @@ suspend fun Connection.executeIO(): Connection.Response = withContext(Dispatcher
 fun String.urlEncode(): String = URLEncoder.encode(this, "utf-8")
 fun String.toUrl(): Uri? = runCatching { Uri.parse(this) }.getOrNull()
 fun String.toUrlBuilder(): Uri.Builder? = toUrl()?.buildUpon()
+fun String.toUrlBuilderSafe(): Uri.Builder = toUrl()?.buildUpon()!!
+fun Uri.Builder.ifCase(case: Boolean, action: Uri.Builder.() -> Uri.Builder) = when {
+    case -> action(this)
+    else -> this
+}
 fun Uri.Builder.addPath(vararg path: String) = path.fold(this) { builder, s ->
     builder.appendPath(s)
 }
