98825c83ff2544292c140bb84c813820b21dd7ba
nani <87673002+nanihadesuka@users.noreply.github.com>
2022-04-08 06:41:53 +0200

    Update gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 		mavenCentral()
 	}
 	dependencies {
-		classpath("com.android.tools.build:gradle:7.1.2")
+		classpath("com.android.tools.build:gradle:7.1.3")
 		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 		classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
 
