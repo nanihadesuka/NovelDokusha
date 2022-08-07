f71b76bc3d014320340789c02943748b7ec345bb
nani <87673002+nanihadesuka@users.noreply.github.com>
2022-08-07 22:30:17 +0200

    Add debug logs for cookies jar

--- app/src/main/java/my/noveldokusha/network/ScraperCookieJar.kt
+++ app/src/main/java/my/noveldokusha/network/ScraperCookieJar.kt
@@ -1,6 +1,8 @@
 package my.noveldokusha.network
 
+import android.util.Log
 import android.webkit.CookieManager
+import my.noveldokusha.BuildConfig
 import okhttp3.Cookie
 import okhttp3.CookieJar
 import okhttp3.HttpUrl
@@ -14,12 +16,18 @@ class ScraperCookieJar : CookieJar {
         val authority = url.toUrl().authority
         val cookies = manager.getCookie(authority)
         return if (cookies != null && cookies.isNotBlank()) {
+            if(BuildConfig.DEBUG){
+                Log.v("CookieJar:loadForRequest", "url:$url\n\nauthority:$authority\n\ncookies:$cookies")
+            }
             cookies.split(";").mapNotNull { Cookie.parse(url, it) }
         } else emptyList()
     }
 
     override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
         val authority = url.toUrl().authority
+        if(BuildConfig.DEBUG){
+            Log.v("CookieJar:saveFromResponse", "url:$url\n\nauthority:$authority\n\ncookies:$cookies")
+        }
         cookies.forEach {
             manager.setCookie(authority, it.toString())
         }
