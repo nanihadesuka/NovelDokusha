f2d897ae713e1c255eae0e09891879781cd1c900
nani <87673002+nanihadesuka@users.noreply.github.com>
2023-04-14 00:17:18 +0200

    Selectable text + divider

--- app/src/main/java/my/noveldokusha/ui/screens/chaptersList/ChaptersScreenHeader.kt
+++ app/src/main/java/my/noveldokusha/ui/screens/chaptersList/ChaptersScreenHeader.kt
@@ -12,11 +12,15 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -28,6 +32,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -146,10 +151,17 @@ fun ChaptersScreenHeader(
                     )
                 }
             }
-            ExpandableText(
-                text = bookState.description,
-                linesForExpand = 4
-            )
+            SelectionContainer {
+                val text by remember(bookState.description) { derivedStateOf { bookState.description.trim() } }
+                ExpandableText(
+                    text = text,
+                    linesForExpand = 4
+                )
+            }
+            Divider(
+                Modifier
+                    .padding(horizontal = 40.dp)
+                    .alpha(0.5f), thickness = Dp.Hairline)
         }
     }
 }
