e5e54b7f0b438cbe32548bd3386e002c3356b475
nani <87673002+nanihadesuka@users.noreply.github.com>
2023-04-26 21:01:36 +0200

    Update android gradle plugin version to 8.2.0 for android studio hedgehog compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 		mavenCentral()
 	}
 	dependencies {
-		classpath("com.android.tools.build:gradle:8.1.0-alpha11")
+		classpath("com.android.tools.build:gradle:8.2.0-alpha01")
 		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
 		classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
 	}
