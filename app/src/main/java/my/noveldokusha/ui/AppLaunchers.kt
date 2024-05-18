c680f0d42b6a59b8a13552fb10427df421d91e15
nani <87673002+nanihadesuka@users.noreply.github.com>
2024-05-18 13:38:37 +0200

    Remove old activity launchers (using navigationRouter now)

--- app/src/main/java/my/noveldokusha/ui/AppLaunchers.kt
+++ app/src/main/java/my/noveldokusha/ui/AppLaunchers.kt
@@ -1,20 +1,11 @@
 package my.noveldokusha.ui
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import my.noveldoksuha.databaseexplorer.databaseBookInfo.DatabaseBookInfoActivity
 import my.noveldoksuha.databaseexplorer.databaseSearch.DatabaseSearchActivity
 import my.noveldoksuha.databaseexplorer.databaseSearch.DatabaseSearchExtras
-import my.noveldokusha.features.chapterslist.ChaptersActivity
-import my.noveldokusha.features.reader.ReaderActivity
-import my.noveldokusha.features.webView.WebViewActivity
 import my.noveldokusha.globalsourcesearch.GlobalSourceSearchActivity
-import my.noveldokusha.mappers.mapToBookMetadata
 import my.noveldokusha.scraper.SourceInterface
-import my.noveldokusha.scraper.domain.BookResult
 import my.noveldokusha.sourceexplorer.SourceCatalogActivity
-import my.noveldokusha.tooling.local_database.BookMetadata
 
 fun Context.goToSourceCatalog(source: SourceInterface.Catalog) {
     SourceCatalogActivity
@@ -33,64 +24,3 @@ fun Context.goToGlobalSearch(text: String) {
         .IntentData(this, text)
         .let(::startActivity)
 }
-
-fun Context.goToReader(bookUrl: String, chapterUrl: String) {
-    ReaderActivity
-        .IntentData(this, bookUrl = bookUrl, chapterUrl = chapterUrl)
-        .let(::startActivity)
-}
-
-fun Context.goToWebBrowser(url: String) {
-    Intent(Intent.ACTION_VIEW)
-        .also { it.data = Uri.parse(url) }
-        .let(::startActivity)
-}
-
-fun Context.goToDatabaseSearch(
-    input: String,
-    databaseUrlBase: String = "https://www.novelupdates.com/"
-) {
-    DatabaseSearchActivity
-        .IntentData(
-            this,
-            DatabaseSearchExtras.Title(databaseBaseUrl = databaseUrlBase, title = input)
-        )
-        .let(::startActivity)
-}
-
-fun Context.goToDatabaseSearchGenres(
-    includedGenresIds: List<String>,
-    databaseUrlBase: String
-) {
-    DatabaseSearchActivity
-        .IntentData(
-            this,
-            DatabaseSearchExtras.Genres(
-                databaseBaseUrl = databaseUrlBase,
-                includedGenresIds = includedGenresIds,
-                excludedGenresIds = listOf()
-            )
-        )
-        .let(::startActivity)
-}
-
-fun Context.goToDatabaseBookInfo(bookMetadata: BookMetadata, databaseUrlBase: String) {
-    DatabaseBookInfoActivity
-        .IntentData(this, databaseUrlBase = databaseUrlBase, bookMetadata = bookMetadata)
-        .let(::startActivity)
-}
-
-
-fun Context.goToBookChapters(bookResult: BookResult) =
-    goToBookChapters(bookMetadata = bookResult.mapToBookMetadata())
-
-fun Context.goToBookChapters(bookMetadata: BookMetadata) {
-    ChaptersActivity
-        .IntentData(this, bookMetadata = bookMetadata)
-        .let(::startActivity)
-}
-
-fun Context.goToWebViewWithUrl(url: String) {
-    WebViewActivity.IntentData(this, url = url).let(::startActivity)
-}
-
